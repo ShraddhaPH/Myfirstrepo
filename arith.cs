64d2f9e629eb543c0658a4dbd5f31165a8b5637b
Shraddha <shraddhaph.97@gmail.com>
2019-08-13 12:16:36 +0530

    new file arith.cs added

---
+++ arith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Arith
+    {
+        public static void Main()
+        {
+            int n1;
+            int n2;
+            Console.WriteLine("enter n1");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter n2");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(n1 + "+" + n2 + "=" + (n1 + n2));
+            Console.WriteLine(n1 + "-" + n2 + "=" + (n1 - n2));
+            Console.WriteLine(n1 + "*" + n2 + "=" + (n1 * n2));
+            Console.WriteLine(n1 + "/" + n2 + "=" + (n1 / n2));
+        }
+    }
+}
